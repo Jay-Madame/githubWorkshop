b99824afb8e0c6ef9d4726a3a849a3deca3c6a70
Jay <jay@Jays-MacBook-Air.local>
2025-01-29 16:04:09 -0600

    Minor change - added a line

--- hello.cpp
+++ hello.cpp
@@ -4,6 +4,6 @@ int main()
 {
     std::cout << "This is just a sample program." << std::endl;
     std::cout << "Hello everybody!" << std::endl;
-    std::cout << "This is a separate line" << std::endl;
+    std::cout << "I am adding a line :)" << std::endl;
     return EXIT_SUCCESS;
 }
